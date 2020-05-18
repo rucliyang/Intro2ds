9acdac6f469398139b5ce2b24286e89db0a12046
Yang Li <rucliyang@gmail.com>
2020-05-18 09:34:57 +0800

    Chap07

---
+++ AI.R
@@ -0,0 +1,115 @@
+�?
+
+setwd("D:\\ai")
+
+# 读入糖尿病数�?
+library(readxl)
+d1 <- read_excel("diabetes.xlsx")
+X <- d1[, names(d1) != "class"]
+y <- factor(d1$class)
+
+
+# 神经网络建模
+library(RSNNS)
+library(caret)
+m1 <- train(X, y, method = "mlp", size = c(3, 2))
+weightMatrix(m1$finalModel)
+
+p1 <- predict(m1, newdata = X)
+confusionMatrix(y, p1, positive = "pos")
+
+prob1 <- predict(m1, newdata = X, type = "prob")$pos
+library(pROC)
+roc1 <- roc(y, prob1)
+roc1
+plot(roc1, print.auc = TRUE, print.thres = TRUE)
+
+
+# 使用 neuralnet �?
+library(neuralnet)
+m1 <- neuralnet(class~pregnant+glucose+pressure+triceps+insulin+mass+pedigree+age, d1, hidden=c(2, 2))
+m1$result.matrix
+
+plot(m1)
+
+prob1 <- compute(m1, d1[, 1:8])$net.result[, 2]
+library(pROC)
+roc1 <- roc(y, prob1)
+roc1
+plot(roc1, print.auc = TRUE, print.thres = TRUE)
+
+p1 <- factor(c("neg", "pos")[as.numeric(prob1 > 0.419) + 1])
+confusionMatrix(y, p1, positive = "pos")
+
+
+
+# MXNet 的基础操作
+
+#cran <- getOption("repos")
+#cran["dmlc"] <- "https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/R/CRAN/"
+#options(repos = cran)
+#install.packages("mxnet")
+
+library(mxnet)
+a <- mx.nd.zeros(c(2, 3))
+a * 2 + 1
+
+x = mx.nd.ones(12)
+y <- mx.nd.array(0:11)
+dim(x)
+
+x1 <- mx.nd.reshape(x, c(3, 4))
+y1 <- mx.nd.reshape(y, c(4, 3))
+
+x1 * x1
+mx.nd.dot(x1, y1)
+
+mx.nd.concat(list(x1, x1), dim = 1)
+mx.nd.concat(list(x1, x1), dim = 0)
+
+mx.nd.slice.axis(y1, axis = 1, begin = 1, end = 3)  #前闭后开
+
+x2 <- mx.nd.array(matrix(1:6, 2, 3))
+class(x2)
+x3 <- as.array(x2)
+class(x3)
+
+
+x4 <- mx.nd.ones(c(3, 4), ctx = mx.gpu())
+x4
+
+
+# 深度学习建模
+X1 <- as.matrix(X)
+y1 <- as.numeric(y) - 1
+
+#m1 <- mx.mlp(X1, y1, hidden_node= c(2, 3), out_node=2, out_activation="softmax", num.round=20, array.batch.size=15, learning.rate=0.1, momentum=0.9, eval.metric = mx.metric.accuracy)
+
+data1 <- mx.symbol.Variable("data")
+fc1 <- mx.symbol.FullyConnected(data = data1, num_hidden = 2, name = "fc1")
+act1 <- mx.symbol.Activation(data = fc1, act_type = "tanh", name = "act1")
+fc2 <- mx.symbol.FullyConnected(data = act1, num_hidden = 3, name = "fc2")
+act2 <- mx.symbol.Activation(data = fc2, act_type = "tanh", name = "act2")
+fc3 <- mx.symbol.FullyConnected(data = act2, num_hidden = 2, name = "fc3")
+mlp <- mx.symbol.SoftmaxOutput(data = fc3, name = "softmax")
+
+graph.viz(mlp, direction = "LR", type = "vis")
+
+mx.set.seed(0)
+m1 <- mx.model.FeedForward.create(mlp, X = X1, y = y1, ctx = mx.cpu(), num.round=200, array.batch.size=5, learning.rate=0.1, optimizer = "sgd", eval.metric = mx.metric.accuracy)
+
+prob1 = predict(m1, X1)[2, ]
+library(pROC)
+roc1 <- roc(y1, prob1)
+roc1
+plot(roc1, print.auc = TRUE, print.thres = TRUE)
+
+library(caret)
+p1 <- factor(c("neg", "pos")[as.numeric(prob1 > 0.5) + 1])
+confusionMatrix(y, p1, positive = "pos")
+
+
+
+
+	
+	
---
+++ AI.py
@@ -0,0 +1,92 @@
+﻿
+
+import os
+os.chdir("D:\\ai")
+
+# 读入糖尿病数据
+import pandas as pd
+d1 = pd.read_excel("diabetes.xlsx")
+X = d1.drop(['class'], axis=1)
+y = d1['class']
+
+# 神经网络建模
+from sklearn.neural_network import MLPClassifier
+m1 = MLPClassifier(hidden_layer_sizes=(3,2)) 
+m1.fit(X, y)
+m1.coefs_
+m1.score(X, y)
+
+p1 = m1.predict(X)
+from sklearn.metrics import classification_report
+print(classification_report(y, p1, labels=['pos', 'neg']))
+
+
+
+# MXNet 的基础操作
+import mxnet as mx
+a = mx.nd.ones((2, 3))
+a * 2 + 1
+
+x = mx.nd.ones(12)
+y = mx.nd.arange(12)
+x.sahpe
+
+x1 = x.reshape((3, 4))
+y1 = y.reshape((4, 3))
+
+x1 * x1
+mx.nd.dot(x1, y1)
+
+mx.nd.concat(x1, x1, dim = 1)
+mx.nd.concat(x1, x1, dim = 0)
+
+y1.slice_axis(axis = 1, begin = 1, end = 3) 
+
+import numpy as np
+x2 = mx.nd.array(np.ones((2, 3)))
+type(x2)
+x3 = x2.asnumpy()
+type(x3)
+
+
+x4 = mx.nd.ones((3,4), ctx = mx.gpu())
+x4 + 1
+
+
+# 深度学习建模
+from sklearn.preprocessing import LabelEncoder
+X1 = X.values
+le1 = LabelEncoder().fit(y.values)
+y1 = le1.transform(y.values)
+
+data1 = mx.sym.var("data")
+fc1 = mx.sym.FullyConnected(data = data1, num_hidden = 2)
+act1 = mx.sym.Activation(data = fc1, act_type = "tanh")
+fc2 = mx.sym.FullyConnected(data = act1, num_hidden = 3)
+act2 = mx.sym.Activation(fc2, act_type = "tanh")
+fc3 = mx.sym.FullyConnected(act2, num_hidden = 2)
+
+mlp = mx.sym.SoftmaxOutput(data = fc3, name = "softmax")
+
+
+# 开启输出记录
+import logging
+logging.getLogger().setLevel(logging.DEBUG) 
+
+# 使用 mx.model.FeedForward.create 运行
+mx.random.seed(0)
+m1 = mx.model.FeedForward.create(mlp, X = X1, y = y1, ctx = mx.cpu(), num_epoch = 200, numpy_batch_size = 5, optimizer='sgd', eval_metric='acc')
+from sklearn.metrics import roc_auc_score
+prob1 = m1.predict(X1)
+roc_auc_score(y1, prob1[:,1])
+
+# 使用 mx.mod.Module 运行
+train_iter = mx.io.NDArrayIter(X1, y1, batch_size = 5)
+m1 = mx.mod.Module(symbol=mlp, context=mx.cpu())
+mx.random.seed(0)
+m1.fit(train_iter, optimizer='sgd', optimizer_params={'learning_rate':0.1}, num_epoch=20, eval_metric='acc')
+                       
+from sklearn.metrics import roc_auc_score
+prob1 = m1.predict(train_iter).asnumpy()
+roc_auc_score(y1, prob1[:,1])
+
